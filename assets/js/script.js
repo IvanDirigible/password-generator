9bfe21b0b5ece9c75e6ca3f45f695d6ca2bad1c7
IvanDirigible <sickeruncible@gmail.com>
2023-09-25 23:07:32 -0400

    Code got too messy. Going to try and rewrite it with more focus/clarity.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 //Adding these up here
-var listBase = ""
+var listBase = []
 var lowerCaseList = "abcdefghijklmnopqrstuvwxyz".split(
   ""
 );
@@ -15,7 +15,11 @@ var specialCharList = " !#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".split(
   ""
 );
 // var ListAmount = 0;
-
+function randomCharSelector (){
+  var randomIndex = Math.floor(Math.random() * listBase.length);
+  var randomChar = listBase[randomIndex];
+  return randomChar;
+}
 // Write password to the #password input
 function writePassword() {
   //Start of work area
@@ -32,45 +36,90 @@ if (!charNum) {
   return; 
 }
 else var lowerCaseQuery = confirm("Would you like to include lowercase letters in your password?");
-if (lowerCaseQuery === false) {
-  lowerCaseList = "";
+if (lowerCaseQuery === true) {
+  listBase.push(lowerCaseList);
 }
+console.log(listBase);
 var upperCaseQuery = confirm("Would you like to include uppercase letters in your password?");
-if (upperCaseQuery === false) {
-  upperCaseList = ""
+if (upperCaseQuery === true) {
+  listBase.push(upperCaseList);
 }
+console.log(listBase);
 var numbersQuery = confirm("Would you like to include numbers in your password?");
-if (numbersQuery === false) {
-  numbersList = ""
+if (numbersQuery === true) {
+  listBase.push(numbersList);
 }
+console.log(listBase);
 var specialCharQuery = confirm("Would you like to include special characters in your password?");
-if (specialCharQuery === false) {
-  specialCharList = ""
+if (specialCharQuery === true) {
+  listBase.push(specialCharList);
 }
+console.log(listBase);
 var passwordConfirm = confirm("You have chosen a password that is " + charNum + " characters long and includes the following: \n Puppies, Kittens, Bears \n Would you like to generate your password?")
 if (passwordConfirm === false) {
   return;
 }
-else if (passwordList = ""){
+else if (listBase == ""){
   alert("Your password does not include any characters! Please try again.");
   return;
 }
-else
+else {
+  alert("Your password has been generated.")
+}
+console.log(listBase);
 // else lowerCaseList.concat(upperCaseList)
 
+// else var lowerCaseQuery = confirm("Would you like to include lowercase letters in your password?");
+// if (lowerCaseQuery === false) {
+//   lowerCaseList = "";
+// }
+// var upperCaseQuery = confirm("Would you like to include uppercase letters in your password?");
+// if (upperCaseQuery === false) {
+//   upperCaseList = ""
+// }
+// var numbersQuery = confirm("Would you like to include numbers in your password?");
+// if (numbersQuery === false) {
+//   numbersList = ""
+// }
+// var specialCharQuery = confirm("Would you like to include special characters in your password?");
+// if (specialCharQuery === false) {
+//   specialCharList = ""
+// }
+// var passwordConfirm = confirm("You have chosen a password that is " + charNum + " characters long and includes the following: \n Puppies, Kittens, Bears \n Would you like to generate your password?")
+// if (passwordConfirm === false) {
+//   return;
+// }
+// else if (passwordList = ""){
+//   alert("Your password does not include any characters! Please try again.");
+//   return;
+// }
+// else
 
-var passwordList = lowerCaseList.concat(upperCaseList, numbersList, specialCharList);
+//This or the other
+// var passwordList = lowerCaseList.concat(upperCaseList, numbersList, specialCharList);
+
 // var passwordList = listBase.concat(lowerCaseList, upperCaseList);
-console.log(passwordList);
+// console.log(passwordList);
+console.log(charNum);
 
-for (var i = 0; i < charNum; i++);
   //End of work area
 
-
-  var password = generatePassword();
+  console.log(password);
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  passwordText.value = password;
 
+  randomChar = randomCharSelector();
+  for (var i = 0; i < listBase.length; i++){
+  }
+  // for (var i = 0; i < charNum; i++) {
+  //   // var password = Math.floor(Math.random() * charNum);
+  //   password += passwordList[Math.floor(Math.random() * charNum)];
+  // };
+
+  passwordText.value = password;
+  console.log(password);
+  // console.log(combination);
+  return password;
 }
 
 // Add event listener to generate button
@@ -79,5 +128,5 @@ generateBtn.addEventListener("click", writePassword);
 // function generatePassword(){
 //   var password = "P6s%hshBD29(@"
 
-//   return password
+  // return password;
 // }
